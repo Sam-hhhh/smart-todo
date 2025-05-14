c89760d6330e8d0cdda0904c509d87d1bdf77150
Sam-hhhh <95973563+Sam-hhhh@users.noreply.github.com>
2025-05-14 18:40:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/smart-todo/', // 仓库名，注意前后斜杠
+  base: '/smart-todo/', // 这里必须和你的 GitHub 仓库名一致！
   plugins: [react()],
 })
