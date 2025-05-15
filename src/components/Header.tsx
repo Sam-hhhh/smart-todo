8ca570fe733cf398d1c4a22c09b226e431ddf2b0
Sam-hhhh <95973563+Sam-hhhh@users.noreply.github.com>
2025-05-15 09:16:19 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import "../styles/Header.scss";
+import "../styles/header.scss";
 import Icon from './Icon'
 const Header = () => {
   return <header className="header"><Icon type="logo"/>智能代办清单</header>
